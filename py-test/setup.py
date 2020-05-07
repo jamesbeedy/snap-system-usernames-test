6948924b7990a9bc7fddc0520339b9f54920f90b
jamesbeedy <jamesbeedy@gmail.com>
2020-05-07 15:41:29 +0000

    init

---
+++ py-test/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="py-test",
+    version="0.0.1",
+    author="BDX",
+    author_email="bdx@bdx.com",
+    description="bdx",
+    scripts=["py-test"],
+)
